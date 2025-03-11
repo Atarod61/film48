0511b46866eb17c7a5ad34df9855b948abacf7ad
Atarod61 <atarod2017@gmail.com>
2025-03-11 09:22:01 +0330

    Refactor code

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "film48/user"
+import (
+	"film48/user"
+	"fmt"
+)
 
-func main(){
-	user.User.ID :=
+func main() {
+	user1 := user.User{ID: 1, Name: "alireza", Email: "alireza@gmail.com"}
+	fmt.Println(user1)
+	user2 := user.SerUser2(1, "alireza", "alireza@gmail.com")
 }
--- user/oneuser.go
+++ user/oneuser.go
@@ -1,8 +1,12 @@
 package user
 
 type User2 struct {
-	ID       int
-	Name     string
-	LastName string
-	Email    string
+	id       int
+	name     string
+	lastName string
+	email    string
+}
+
+func SerUser2(id int, name string, email string) User2 {
+	return User2{id: id, name: name, email: email}
 }
