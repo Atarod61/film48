ef5c4ea5b4eba3a6b238021ef22b6a93b7e68518
Atarod61 <atarod2017@gmail.com>
2025-03-11 09:22:50 +0330

    Refactor code

--- main.go
+++ main.go
@@ -9,4 +9,5 @@ func main() {
 	user1 := user.User{ID: 1, Name: "alireza", Email: "alireza@gmail.com"}
 	fmt.Println(user1)
 	user2 := user.SerUser2(1, "alireza", "alireza@gmail.com")
+	fmt.Println(user2)
 }
